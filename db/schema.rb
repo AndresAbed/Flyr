7134a9d8db293bc29f1a6ce16972d0cedeb1fc77
Andres Abed <abedandres@gmail.com>
2014-10-08 11:03:01 -0300

    Pg db configured

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007202345) do
+ActiveRecord::Schema.define(version: 20141007185921) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
     t.string   "provider"
