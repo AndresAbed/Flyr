66bf7012c0b8206f93c1aa8fae9071e461b3699b
AndresAbed <abedandres@gmail.com>
2014-10-26 23:44:11 -0300

    Home route changed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,15 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+(function() {
+    "use strict";
+    if (window.location && window.location.hash) {
+        if (window.location.hash === '#_=_') {
+            window.location.hash = '';
+            return;
+        }
+        var facebookFubarLoginHash = RegExp('_\=_', 'g');
+        window.location.hash = window.location.hash.replace(facebookFubarLoginHash,     '');
+    }
+}()); 
