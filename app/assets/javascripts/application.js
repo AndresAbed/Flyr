189f2f9180ab961e325a522fcaa1ba54b6cdcff9
AndresAbed <abedandres@gmail.com>
2014-10-27 17:00:37 -0300

    Facebook hash removed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,14 +16,3 @@
 //= require turbolinks
 //= require_tree .
 
-(function() {
-    "use strict";
-    if (window.location && window.location.hash) {
-        if (window.location.hash === '#_=_') {
-            window.location.hash = '';
-            return;
-        }
-        var facebookFubarLoginHash = RegExp('_\=_', 'g');
-        window.location.hash = window.location.hash.replace(facebookFubarLoginHash,     '');
-    }
-}()); 
