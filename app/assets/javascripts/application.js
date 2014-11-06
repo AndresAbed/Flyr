ea9b9f19dfcfaae3ab1a6ba10363bc850330dcfa
AndresAbed <abedandres@gmail.com>
2014-11-05 23:57:27 -0300

    Main view frontend Scroll to top arrow Button ingresar Sign_in form animated Turbolinks error fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,104 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require turbolinks
 //= require_tree .
 
+/* Sign_in form */
+
+$(document).ready(function(){
+	animationClick('#sign_in', '#li', 'fadeInDownBig')
+
+	function animationClick(element, element2, animation){
+	  $element = $(element);
+	  $element2 = $(element2);
+	  $element2.click(
+	    function() {
+	      $element.addClass('animated ' + animation);
+	      $element.css({
+	      	"position":"absolute", 
+	      	"display":"block", 
+	      	"margin-top":"90px", 
+	      	"margin-left":"auto", 
+	      	"margin-right":"auto", 
+	      	"left":"0", 
+	      	"right":"0"
+	      });
+	      //wait for animation to finish before removing classes
+	      window.setTimeout( function(){
+	          $element.removeClass('animated ' + animation);
+	      }, 1000);
+	    }
+	  );
+	};
+})
+
+$(document).ready(function(){
+	animationClick('#sign_in', '#close', 'fadeOutUp')
+
+	function animationClick(element, element2, animation){
+	  $element = $(element);
+	  $element2 = $(element2);
+	  $element2.click(
+	    function() {
+	      $element.addClass('animated ' + animation);
+	      $element.css({
+	      	"position":"absolute", 
+	      	"display":"block",  
+	      	"margin-left":"auto", 
+	      	"margin-right":"auto", 
+	      	"left":"0", 
+	      	"right":"0",
+	      });
+	      //wait for animation to finish before removing classes
+	      window.setTimeout( function(){
+	          $element.removeClass('animated ' + animation);
+	          $element.hide();
+	      }, 1000);
+	    }
+	  );
+	};
+})
+
+/* Sign_in button*/
+
+$(document).ready(function(){
+	$('#li').addClass('animated zoomIn');
+	window.setTimeout( function(){
+	    $('#li').removeClass('animated zoomIn');
+	}, 1000);
+})
+
+$(document).ready(function(){
+	$('.navbar-collapse').on('shown.bs.collapse', function(){
+		$('#li').hide(1000);
+	})
+	$('.navbar-toggle').click(function() {
+        $("#li").show();
+        $('#li').addClass('animated zoomIn');
+    });
+    window.setTimeout( function(){
+	    $('#li').removeClass('animated zoomIn');
+	}, 1000);
+})
+
+
+/* Scroll to top */ 
+
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('#scrollToTop').fadeIn();
+		} else {
+			$('#scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('#scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},1000);
+		return false;
+	});
+	
+});
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
      $("#myCarousel").carousel({
-         interval : 5000
+         interval : 5000,
+         pause:false
      });
 });
