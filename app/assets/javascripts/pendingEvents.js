9dc39a74b08d559bac64a8fd23d0570c31ac4352
AndresAbed <abedandres@gmail.com>
2015-04-08 05:08:00 -0300

    Adds a get request to pending events Adds a get request before the interval to load the pending events

--- app/assets/javascripts/pendingEvents.js
+++ app/assets/javascripts/pendingEvents.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
+  $.get( "/clubevents/pending", function( data ) {
+      $("#pendingEvents").html("");
+      data.clubevents.forEach(function(data){
+        $("<li><a href="+data.url+">"+data.name+"</a></li>").appendTo("#pendingEvents");
+        $("<li class= divider></li>").appendTo("#pendingEvents");
+      });
+      $("#pendingCount").text(data.clubevents.length)
+      if (data.clubevents.length == 0) {
+        $("<li>No hay eventos</li>").appendTo("#pendingEvents");
+      };
+    });
 	setInterval(function(){
 		$.get( "/clubevents/pending", function( data ) {
 		  $("#pendingEvents").html("");
