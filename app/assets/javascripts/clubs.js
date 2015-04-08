abe17bc11ceded6e432c47df52770c8ed34bff2d
AndresAbed <abedandres@gmail.com>
2015-04-08 05:04:11 -0300

    Refactor the music edit js code

--- app/assets/javascripts/clubs.js
+++ app/assets/javascripts/clubs.js
@@ -24,24 +24,28 @@ $(document).ready(function(){
   });
 });
 
-/* Musics */
+/* Music */
+
+var editMusic = function() {
+  $("#music-edit-text").text("Terminar");
+  $("#music-pencil").removeClass('fa fa-pencil');
+  $("#music-pencil").addClass('fa fa-check');
+  $("#music-add").fadeIn();
+  $(".fa-times").fadeIn();
+};
+
+var finishEdition = function() {
+  $("#music-edit-text").text("Editar");
+  $("#music-pencil").removeClass('fa fa-check');
+  $("#music-pencil").addClass('fa fa-pencil');
+  $("#music-add").fadeOut();
+  $(".fa-times").fadeOut();
+}
 
 $(document).ready(function(){
   $("#music-edit").on("click", function() {
     var text = $("#music-edit-text").text();
-    if (text == "Editar"){
-      $("#music-edit-text").text("Terminar");
-      $("#music-pencil").removeClass('fa fa-pencil');
-      $("#music-pencil").addClass('fa fa-check');
-      $("#music-add").fadeIn();
-      $(".fa-times").fadeIn();
-    }
-    else{
-      $("#music-edit-text").text("Editar");
-      $("#music-pencil").removeClass('fa fa-check');
-      $("#music-pencil").addClass('fa fa-pencil');
-      $("#music-add").fadeOut();
-      $(".fa-times").fadeOut();
-    };
+    if (text == "Editar"){editMusic();}
+    else{finishEdition();};
   });
-});
+})
