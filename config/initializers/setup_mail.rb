7ef9e7062d131e9fced41b74485dd0382a2782e3
AndresAbed <abedandres@gmail.com>
2015-04-29 02:27:20 -0300

    Adds Sendgrid configuration

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
 	:address => 'smtp.sendgrid.net',
 	:port => '587',
 	:authentication => :plain,
-	:user_name => 'app30785322@heroku.com',
-	:password =>  'qpinawpp',
+	:user_name => 'app36349476@heroku.com',
+	:password =>  'fksnj2mx3045',
 	:domain => 'heroku.com',
 	:enable_starttls_auto => true
 }
