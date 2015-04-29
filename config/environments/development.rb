53291cd8af27eed985677f22b8978d8be32bcbd7
AndresAbed <abedandres@gmail.com>
2015-04-29 02:38:13 -0300

    Adds production host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,5 +37,6 @@ Rails.application.configure do
   Paperclip.options[:command_path] = "/usr/bin/convert"
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 end
