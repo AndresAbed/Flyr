0c415e752cfc1ace845da9e6309d987194fcfec6
AndresAbed <abedandres@gmail.com>
2015-06-30 18:21:48 -0300

    Modifies some clubs.js functions and adds new functions for profile's buttons

--- app/assets/javascripts/clubs.js
+++ app/assets/javascripts/clubs.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
     }
   });
 
-  $(".logo-container").fancybox({
+  $(".club-logo").fancybox({
     maxWidth: 1400,
     maxHeight: 1400,
     openEffect: 'elastic',
@@ -24,28 +24,90 @@ $(document).ready(function(){
   });
 });
 
-/* Music */
+/* Features & Music */
+
+var editFeature = function() {
+  $("#feature-edit-text").text("Terminar");
+  $("#feature-pencil").removeClass('fa fa-pencil');
+  $("#feature-pencil").addClass('fa fa-check');
+  $("#feature-add").fadeIn();
+  $(".features-list .fa-times").fadeIn();
+};
+
+var finishFeatureEdition = function() {
+  $("#feature-edit-text").text("Editar");
+  $("#feature-pencil").removeClass('fa fa-check');
+  $("#feature-pencil").addClass('fa fa-pencil');
+  $("#feature-add").fadeOut();
+  $(".features-list .fa-times").fadeOut();
+}
 
 var editMusic = function() {
   $("#music-edit-text").text("Terminar");
   $("#music-pencil").removeClass('fa fa-pencil');
   $("#music-pencil").addClass('fa fa-check');
   $("#music-add").fadeIn();
-  $(".fa-times").fadeIn();
+  $(".music-list .fa-times").fadeIn();
 };
 
-var finishEdition = function() {
+var finishMusicEdition = function() {
   $("#music-edit-text").text("Editar");
   $("#music-pencil").removeClass('fa fa-check');
   $("#music-pencil").addClass('fa fa-pencil');
   $("#music-add").fadeOut();
-  $(".fa-times").fadeOut();
+  $(".music-list .fa-times").fadeOut();
 }
 
 $(document).ready(function(){
+  $(".music").mouseover(function(){
+    $(".music-btns").addClass("music-btns-show");
+  });
+  $(".music").mouseout(function(){
+    var text = $("#music-edit-text").text();
+    if (text == "Editar") {
+      $(".music-btns").removeClass("music-btns-show");
+    };
+  });
   $("#music-edit").on("click", function() {
     var text = $("#music-edit-text").text();
     if (text == "Editar"){editMusic();}
-    else{finishEdition();};
+    else{finishMusicEdition();};
+  });
+
+  $(".features").mouseover(function(){
+    $(".features-btns").addClass("features-btns-show");
+  });
+  $(".features").mouseout(function(){
+    var text = $("#feature-edit-text").text();
+    if (text == "Editar") {
+      $(".features-btns").removeClass("features-btns-show");
+    };
+  });
+  $("#feature-edit").on("click", function() {
+    var text = $("#feature-edit-text").text();
+    if (text == "Editar"){editFeature();}
+    else{finishFeatureEdition();};
+  });
+})
+
+/* Profile buttons */
+
+$(document).ready(function(){
+  $(".club-logo-info").mouseover(function(){
+    $(".club-logo-info .buttons").addClass("club-buttons-show");
+  });
+  $(".club-logo-info").mouseout(function(){
+    $(".club-logo-info .buttons").removeClass("club-buttons-show");
+  });
+})
+
+/* Events */
+
+$(document).ready(function(){
+  $(".club-event-item").mouseover(function(){
+    $(this).find("span").addClass("club-event-span-up");
+  });
+  $(".club-event-item").mouseout(function(){
+    $(this).find("span").removeClass("club-event-span-up");
   });
 })
