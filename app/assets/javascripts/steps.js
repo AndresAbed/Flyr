3eac511883bee2466a8fa5540af4d40b1c842d62
AndresAbed <abedandres@gmail.com>
2015-07-01 18:32:58 -0300

    Adds location function for steps.js

--- app/assets/javascripts/steps.js
+++ app/assets/javascripts/steps.js
@@ -1,21 +1,25 @@
 $(document).ready(function(){
-  var $steps = $(".steps");
-	var offset = $steps.offset();
-	var $num = $(".numbers");
-	$(this).scroll(function(){
-		if ($(this).scrollTop() > (offset.top)-300) {
-			$num.each(function(i) {
-			  var $el = $(this);
-			  setTimeout(function() {
-			  	$el.css({"opacity":"1"})
-			    $el.addClass('animated fadeInUp');
-			  }, 150 * i);
-			});
-      var $title = $steps.find(".title")
-      $title.css({"opacity":"1"});
-      $title.addClass("animated fadeInLeft");
-      $title.find("i").css({"opacity":"1"});
-      $title.find("i").addClass("animated fadeInLeft");
-		};
-	});
+  var currLoc = location.href.toLowerCase();
+  if (currLoc == "http://localhost:3000/"
+      || currLoc == "http://www.flyr.com.ar/") {
+    var $steps = $(".steps");
+  	var offset = $steps.offset();
+  	var $num = $(".numbers");
+  	$(this).scroll(function(){
+  		if ($(this).scrollTop() > (offset.top)-300) {
+  			$num.each(function(i) {
+  			  var $el = $(this);
+  			  setTimeout(function() {
+  			  	$el.css({"opacity":"1"})
+  			    $el.addClass('animated fadeInUp');
+  			  }, 150 * i);
+  			});
+        var $title = $steps.find(".title")
+        $title.css({"opacity":"1"});
+        $title.addClass("animated fadeInLeft");
+        $title.find("i").css({"opacity":"1"});
+        $title.find("i").addClass("animated fadeInLeft");
+  		};
+  	});
+  };  
 })
